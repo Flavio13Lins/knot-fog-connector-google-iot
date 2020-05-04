a5fdf875c54ee2a86f0a3314f6c37bb0be291850
João Neto <joao.alexandre@cesar.org.br>
2020-05-04 19:16:05 -0300

    Add operation placeholders

---
+++ src/Connector.js
@@ -0,0 +1,46 @@
+class Connector {
+  constructor(settings) { // eslint-disable-line no-useless-constructor, no-unused-vars
+  }
+
+  async start() { // eslint-disable-line no-empty-function
+  }
+
+  async addDevice(device) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  async removeDevice(id) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  async listDevices() { // eslint-disable-line no-empty-function
+  }
+
+  // Device (fog) to cloud
+
+  async publishData(id, dataList) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  async updateSchema(id, schemaList) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  async updateProperties(id, properties) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  // Cloud to device (fog)
+
+  // cb(event) where event is { id, sensorId }
+  async onDataRequested(cb) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  // cb(event) where event is { id, sensorId, data }
+  async onDataUpdated(cb) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  // Connection callbacks
+  async onDisconnected(cb) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+
+  async onReconnected(cb) { // eslint-disable-line no-empty-function, no-unused-vars
+  }
+}
+
+export { Connector }; // eslint-disable-line import/prefer-default-export
