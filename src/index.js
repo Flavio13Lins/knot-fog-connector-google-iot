0a58cff995a3869ddf8cfbe08dd48c5b4307ed3b
Flavio13Lins <flmn@cin.ufpe.br>
2020-07-29 10:29:38 -0300

    fix eslint pre-commit errors in index

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-import { Connector } from './Connector.js';
-
-const settings = { // Teste do Connector
-  iota: {
-      hostname: "localhost",
-      port: "8080"
-  }
-};
-const tryconnector = new Connector(settings); //usando construtor
-const devices = tryconnector.listDevices(); //listando devices
-devices.then(
-function(responses){
-  if(responses){
-    console.log(`${responses.length} Devices found:`)
-  }
-  for (const response of responses){
-    console.log(response);
-  }
-}
-)
-
-const add = tryconnector.addDevice({ id:'another-two', name: 'two'});
-add.then(function(resp){
-  if(resp){
-    console.log(resp);
-  }
-})
+import { Connector } from './Connector';
 
 export default Connector;
